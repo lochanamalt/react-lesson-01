724632f0aeffc74c7d6e46e33b1865e0e4c2d299
lochanamalt <lochanatharanganie@gmail.com>
2023-09-04 09:34:59 +0530

    navbar component renamed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Message from './Message'
-import Navbar from './component/NavBar'
+import Navbar from './component/Navbar'
 import Main from './component/Main'
 
 function App() {
