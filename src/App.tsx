94894a53ac93e7d0211ace330a0b3d638fd2c837
lochanamalt <lochanatharanganie@gmail.com>
2023-09-05 14:47:32 +0530

    added side pane

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Message from './Message'
 import Navbar from './component/Navbar'
 import Main from './component/Main'
+import Content from './component/Content'
+import Footer from './component/Footer'
 
 function App() {
   return (
-  <div>
+  <div className='app-div'>
     <Navbar/>
-    <Main/>
+    <Content/>
+    <Footer/>
   </div>)
 }
 
